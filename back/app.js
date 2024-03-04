b7989e06243ed63daed87ca2f0700cf1b6c179a8
MelwinC <contact@melwinchenu.fr>
2024-03-04 15:46:40 +0100

    config port listening - restore bin/www config & delete the app.js config

--- back/app.js
+++ back/app.js
@@ -41,12 +41,6 @@ require("./routes/auth.routes")(app);
 require("./routes/user.routes")(app);
 require("./routes/chaussette.routes")(app);
 
-// set port, listen for requests
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
-
 // la fonction initials créé 3 enregistrements dans table role de la BDD
 function initial() {
   Role.create({
